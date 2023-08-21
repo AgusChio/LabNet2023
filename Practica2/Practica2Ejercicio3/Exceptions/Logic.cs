598b8a6b3881893bff035b9e5b86ac8675cd3523
Agustina Chiodini <agus.chio25790@gmail.com>
2023-08-21 16:07:59 -0300

    add activity 3

---
+++ Practica2/Practica2Ejercicio3/Exceptions/Logic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica2Ejercicio3.Exceptions
+{
+    internal class Logic
+    {
+        public static void ThrowException()
+        {
+            throw new Exception();
+        }
+    }
+}
---
+++ Practica2/Practica2Ejercicio3/Program.cs
@@ -0,0 +1,28 @@
+﻿using Practica2Ejercicio3.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica2Ejercicio3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Logic.ThrowException();
+            }
+
+            catch (Exception)
+            {
+                Console.WriteLine("Ajá atrape una excepcion, vas a mi coleccion de excepciones muajajaja");
+            }
+
+            Console.WriteLine("La captura llego a su fin, mañana sera otro dia para capturar mas! Si desea salir presione ENTER");
+            Console.ReadLine();
+        }
+    }
+}
